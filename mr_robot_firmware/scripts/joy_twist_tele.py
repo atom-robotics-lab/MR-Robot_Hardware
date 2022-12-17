1e1023661562ddf639b104c62078a2cd29a72e9c
NOEMOJI041 <aloksharma041@gmail.com>
2022-12-17 23:13:00 +0530

    twist class

--- mr_robot_firmware/scripts/joy_twist_tele.py
+++ mr_robot_firmware/scripts/joy_twist_tele.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python3
-from matplotlib.pyplot import axes
-from matplotlib.widgets import Button
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
+from termcolor import colored
+
+colors = ['red', 'green', 'cyan', 'blue', 'magenta']
+class TwistJoy:
+    def __init__(self):        
+        rospy.init_node('Joy2Turtle')
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)    
+        rospy.Subscriber("/joy", Joy, self.callback)    
+        self.axes=0
+        self.buttons=0
+        self.aL=0
+        self.aR=0
+        self.L1 = 0
+        self.R1 = 0
+        self.start()
 
 
-
- 
-
-
-def callback(data):    
-    twist = Twist()    
-    twist.linear.x = 1.15*data.axes[1]    
-    twist.angular.z = 2*data.axes[0]
-    if data.buttons[4] == 1:    
-        twist.angular.z = 2*data.buttons[4]
-    elif data.buttons[5] == 1:    
-        twist.angular.z = -2*data.buttons[5]
-
-    
-    print(data)
-    pub.publish(twist)
+    def callback(self,data):    
+        self.axes = data.axes
+        self.buttons = data.buttons
+        self.aL = self.axes[1]
+        self.aR = self.axes[0]
+        self.R1 = self.buttons[5]
+        self.L1 = self.buttons[4]
 
 
-def start():    
-    global pub
-    pub = rospy.Publisher('/cmd_vel', Twist)    
-    rospy.Subscriber("joy", Joy, callback)    
-    rospy.init_node('Joy2Turtle')
-    rospy.spin()
-
-if __name__ == '__main__':
-    start()
+    def start(self):
+        rate = rospy.Rate(15)
+        twist = Twist()
+        while not rospy.is_shutdown():
+            twist.linear.x = 1.15*self.aL
+            twist.angular.z = 2*self.aR
+            if self.L1 == 1:    
+                twist.angular.z = 2*self.L1
+            elif self.R1 == 1:    
+                twist.angular.z = -2*self.R1 
+            self.pub.publish(twist)
+            rate.sleep()
